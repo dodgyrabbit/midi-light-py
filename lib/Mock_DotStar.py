6d8c43dc786259d6ded241d7a59b5a743940c92a
Pieter <dodgyrabbit@hotmail.com>
2016-12-13 22:13:00 -0500

    Fix bug where I kept on creating new gfx objects

--- lib/Mock_DotStar.py
+++ lib/Mock_DotStar.py
@@ -13,11 +13,12 @@ class Adafruit_DotStar:
     __win = None
     __pixels = None
     __numPixels = 0
+    __leds = []
 
     def __init__(self, numPixels):
         print "Inside initializer"
         self.__numPixels = numPixels
-        self.__pixels = range(0, numPixels)
+        self.__pixels = [0] * numPixels
         print self.__pixels
 
     def getPixel(self, index):
@@ -28,20 +29,6 @@ class Adafruit_DotStar:
         print "begin called in Mock_DotStar"
         # Width of window is the number of keys * (diameter + spacing) and then add a diameter on each side for spacing
         self.__win = GraphWin("PianoPy", self. __numPixels * self.__LED_RADIUS * 2 +  self.__numPixels * (self.__LED_RADIUS/2) + 4 * self.__LED_RADIUS, self.__WINDOW_HEIGHT)
-
-    def setBrightness(self, brightness):
-        print "setBrightness called in Mock_DotStar"
-
-    def setPixelColor(self, index, r, g, b):
-        self.__pixels[index] = b + g << 8 + r << 16
-
-    def clear(self):
-        print "Clearing strip data"
-        # Set strip data to 'off' (just clears buffer, does not write to strip)
-        # There's probably a better way of doing this... this creates a new list I think
-        self.__pixels = range(0, self.__numPixels, 0)
-
-    def show(self):
         x = 2 * self.__LED_RADIUS
         y = self.__WINDOW_HEIGHT // 2
         for pixel in self.__pixels:
@@ -53,4 +40,26 @@ class Adafruit_DotStar:
             c.setFill(color_rgb(r, g, b))
 
             c.draw(self.__win)
+            self.__leds.append(c)
             x += self.__LED_RADIUS * 2 + self.__LED_RADIUS / 2
+
+    def setBrightness(self, brightness):
+        print "setBrightness called in Mock_DotStar"
+
+    def setPixelColorRGB(self, index, r, g, b):
+        self.__pixels[index] = b + g << 8 + r << 16
+
+    def setPixelColor(self, index, color):
+        self.__pixels[index] = color
+
+    def clear(self):
+        print "Clearing strip data"
+        # Set strip data to 'off' (just clears buffer, does not write to strip)
+        self.__pixels = [0] * self.__numPixels
+
+    def show(self):
+        for led, pixel in zip(self.__leds, self.__pixels):
+            b = pixel & 255
+            g = (pixel >> 8) & 255
+            r = (pixel >> 16) & 255
+            led.setFill(color_rgb(r, g, b))
--- main.py
+++ main.py
@@ -2,17 +2,47 @@
 The main beast is alive.
 """
 
-from lib.Mock_DotStar import Adafruit_DotStar
 import time
+from lib.Mock_DotStar import Adafruit_DotStar
 
-strip = Adafruit_DotStar(88)
-strip.begin()
-strip.show()
+# How many keys there are on (your) piano/keyboard
+PIANO_KEYS = 88
 
-try:
-    while True:
-        time.sleep(1)
+# The first note (far left) on your keyboard
+FIRST_MIDI_NOTE = 21
 
-except KeyboardInterrupt:
-    ## Close mock library (which should close window)
-    pass
+def strand_test():
+    """ Basically the same as the Adafruit stand test """
+
+    strip = Adafruit_DotStar(88)
+    strip.begin()
+    strip.show()
+
+    head = 0                                # Index of first 'on' pixel
+    tail = -10                              # Index of last 'off' pixel
+    color = 0xFF0000                        # 'On' color (starts red)
+
+    while True:                             # Loop forever
+        strip.setPixelColor(head, color)    # Turn on 'head' pixel
+        strip.setPixelColor(tail, 0)        # Turn off 'tail'
+        strip.show()                        # Refresh strip
+        time.sleep(1.0 / 50)                # Pause 20 milliseconds (~50 fps)
+        head += 1                           # Advance head position
+        if head >= PIANO_KEYS:              # Off end of strip?
+            head = 0                        # Reset to start
+            color >>= 8                     # Red->green->blue->black
+            if color == 0:
+                color = 0xFF0000            # If black, reset to red
+        tail += 1                           # Advance tail position
+        if tail >= PIANO_KEYS:
+            tail = 0                        # Off end? Reset
+
+strand_test()
+
+# try:
+#     while True:
+#         time.sleep(1)
+
+# except KeyboardInterrupt:
+#     ## Close mock library (which should close window)
+#     pass
