5057e14c71b2c5cc6eea470f64396ed72062950e
Pieter Venter <pieterventer@geotab.com>
2016-12-13 20:18:11 -0500

    Add some more method implementations to mock lib

--- lib/Mock_DotStar.py
+++ lib/Mock_DotStar.py
@@ -3,16 +3,54 @@ A drop in replacment for the Adafruit_DotStar module.
 It allows me to to visualize what the LED strip may look like, without actually having one.
 """
 
-from graphics import GraphWin, Circle, Point
+from graphics import GraphWin, Circle, Point, color_rgb
 
 class Adafruit_DotStar:
     "A mock implementation of the Adafruit_DotStart that simulates LEDs in the UI"
-    def __init__(self, nleds, datapin=None, clockpin=None):
-        pass
+
+    __LED_RADIUS = 5
+    __WINDOW_HEIGHT = 300
+    __win = None
+    __pixels = None
+    __numPixels = 0
+
+    def __init__(self, numPixels):
+        print "Inside initializer"
+        self.__numPixels = numPixels
+        self.__pixels = range(0, numPixels)
+        print self.__pixels
+
+    def getPixel(self, index):
+        print "getPixel called in Mock_DotStar"
+        return self.__pixels(index)
+
     def begin(self):
-        "Mock - Initialize pins/SPI for output. Here it opens a window."
-        win = GraphWin("My Circle", 100, 100)
-        circle = Circle(Point(50, 50), 10)
-        circle.draw(win)
-        win.getMouse() # Pause to view result
-        win.close()    # Close window when done
+        print "begin called in Mock_DotStar"
+        # Width of window is the number of keys * (diameter + spacing) and then add a diameter on each side for spacing
+        self.__win = GraphWin("PianoPy", self. __numPixels * self.__LED_RADIUS * 2 +  self.__numPixels * (self.__LED_RADIUS/2) + 4 * self.__LED_RADIUS, self.__WINDOW_HEIGHT)
+
+    def setBrightness(self, brightness):
+        print "setBrightness called in Mock_DotStar"
+
+    def setPixelColor(self, index, r, g, b):
+        self.__pixels[index] = b + g << 8 + r << 16
+
+    def clear(self):
+        print "Clearing strip data"
+        # Set strip data to 'off' (just clears buffer, does not write to strip)
+        # There's probably a better way of doing this... this creates a new list I think
+        self.__pixels = range(0, self.__numPixels, 0)
+
+    def show(self):
+        x = 2 * self.__LED_RADIUS
+        y = self.__WINDOW_HEIGHT // 2
+        for pixel in self.__pixels:
+            c = Circle(Point(x, y), self.__LED_RADIUS)
+
+            b = pixel & 255
+            g = (pixel >> 8) & 255
+            r = (pixel >> 16) & 255
+            c.setFill(color_rgb(r, g, b))
+
+            c.draw(self.__win)
+            x += self.__LED_RADIUS * 2 + self.__LED_RADIUS / 2
--- lib/main.py
+++ lib/main.py
@@ -3,6 +3,16 @@ The main beast.
 """
 
 from Mock_DotStar import Adafruit_DotStar
+import time
 
-strip = Adafruit_DotStar(10)
+strip = Adafruit_DotStar(88)
 strip.begin()
+strip.show()
+
+try:
+    while True:
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    ## Close mock library (which should close window)
+    pass
